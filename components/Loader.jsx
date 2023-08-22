4a89c3db353da05ebd24b91a00baa2f8e765a32c
vedant.medisage <vedant.medisage@gmail.com>
2023-08-22 14:27:44 +0530

    loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,7 +4,7 @@ import Lottie from 'react-lottie';
 
 function Loader() {
   return (
-    <div className="fixed overflow-scroll scroll  scrollbar-none backdrop-blur-[2px] backdrop-brightness-50 top-0 right-0 left-0 bottom-0 z-[100]">
+    <div className="fixed overflow-scroll scroll  scrollbar-none backdrop-blur-[2px] backdrop-brightness-50 top-0 right-0 left-0 bottom-0 z-[99000]">
       <div className="w-20 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
         <Lottie options={loaderOptions} />
       </div>
