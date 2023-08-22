4eb99274b4bc7ed5caa0efb08ad28673db1977ec
vedant.medisage <vedant.medisage@gmail.com>
2023-08-22 14:41:10 +0530

    top and bottom

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -207,7 +207,7 @@ function Chat({ data }) {
             <BsThreeDotsVertical className="ml-auto cursor-pointer" />
           </div>
           {!isLoading && (
-            <ScrollToBottom className="flex-1 z-50 overflow-auto p-4 md:t-0 t-[72px] md:b-0 b-[64px]">
+            <ScrollToBottom className="flex-1 z-50 overflow-auto p-4 md:top-0 top-[72px] md:bottom-0 bottom-[64px]">
               <ChatSection allMessages={allAllMessages} />
             </ScrollToBottom>
           )}
